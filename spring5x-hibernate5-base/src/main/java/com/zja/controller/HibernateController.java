bf28acf6e25a73aad758d606dd6b6bd8414cfdc9
zhengja <zhengja@dist.com.cn>
2019-11-21 17:53:38 +0800

    feat(hibernate5): 新增测试

--- spring5x-hibernate5-base/src/main/java/com/zja/controller/HibernateController.java
+++ spring5x-hibernate5-base/src/main/java/com/zja/controller/HibernateController.java
@@ -5,6 +5,7 @@ import com.zja.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,21 +15,151 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("rest/hibernate")
-@Api(tags = {"HibernateController"}, description = "Hibernate 基础搭建")
+@Api(tags = {"HibernateController"}, description = "HibernateTemplate 常用方法使用")
 public class HibernateController {
 
     @Autowired
+    @Qualifier("userServiceImpl")
     private UserService userService;
 
-    @GetMapping("getuser")
-    @ApiOperation(value = "获取用户信息", notes = "hibernate", httpMethod = "GET")
-    public Object getuser(@RequestParam Integer id) {
-        return this.userService.getuser(id);
+    @PostMapping("saveEntity")
+    @ApiOperation(value = "save保存用户", notes = "HibernateTemplate", httpMethod = "POST")
+    public Object saveEntity(@RequestBody UserEntity userEntity) {
+        return this.userService.saveEntity(userEntity);
     }
 
-    @PostMapping("saveUser")
-    @ApiOperation(value = "保存用户信息", notes = "hibernate", httpMethod = "POST")
-    public Object saveUser(@RequestBody UserEntity userEntity) {
-        return this.userService.saveUser(userEntity);
+    @PostMapping("saveEntity2")
+    @ApiOperation(value = "save保存用户", notes = "HibernateTemplate", httpMethod = "POST")
+    public Object saveEntity2(@RequestBody UserEntity userEntity) {
+        return this.userService.saveEntity2(userEntity);
     }
+
+    @PostMapping("mergeEntity")
+    @ApiOperation(value = "merge保存用户", notes = "HibernateTemplate", httpMethod = "POST")
+    public Object mergeEntity(@RequestBody UserEntity userEntity) {
+        return this.userService.mergeEntity(userEntity);
+    }
+
+    @PostMapping("mergeEntity2")
+    @ApiOperation(value = "merge保存用户", notes = "HibernateTemplate", httpMethod = "POST")
+    public Object mergeEntity2(@RequestBody UserEntity userEntity) {
+        return this.userService.mergeEntity2(userEntity);
+    }
+
+
+    @GetMapping("getEntity")
+    @ApiOperation(value = "get获取用户信息", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object getEntity(@RequestParam Integer id) {
+        return this.userService.getEntity(id);
+    }
+
+    @GetMapping("loadEntity")
+    @ApiOperation(value = "load获取用户信息-失败", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object loadEntity(@RequestParam Integer id) {
+        return this.userService.loadEntity(id);
+    }
+
+    @GetMapping("getListEntity")
+    @ApiOperation(value = "get获取用户信息", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object getListEntity() {
+        return this.userService.getListEntity();
+    }
+    @GetMapping("getEntityName")
+    @ApiOperation(value = "get根据id获取用户信息-失败", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object getEntityName(@RequestParam Integer id) {
+        return this.userService.getEntityName(id);
+    }
+
+    @GetMapping("getPageByConditions")
+    @ApiOperation(value = "分页查询", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object getPageByConditions() {
+        return this.userService.getPageByConditions();
+    }
+
+    @PutMapping("updateEntity")
+    @ApiOperation(value = "更新用户", notes = "HibernateTemplate", httpMethod = "PUT")
+    public Object updateEntity(@RequestBody UserEntity userEntity) {
+        this.userService.updateEntity(userEntity);
+        return "成功";
+    }
+
+    @PutMapping("saveOrUpdate")
+    @ApiOperation(value = "更新或保存用户信息", notes = "HibernateTemplate", httpMethod = "PUT")
+    public Object saveOrUpdate(@RequestBody UserEntity userEntity) {
+        this.userService.saveOrUpdate(userEntity);
+        return "成功";
+    }
+
+    @DeleteMapping("deleteByEntity")
+    @ApiOperation(value = "根据id删除数据", notes = "HibernateTemplate", httpMethod = "DELETE")
+    public Object deleteByEntity(@RequestParam Integer id) {
+        this.userService.deleteByEntity(id);
+        return "成功";
+    }
+
+    @DeleteMapping("deleteAll")
+    @ApiOperation(value = "删除所有数据", notes = "HibernateTemplate", httpMethod = "DELETE")
+    public Object deleteAll() {
+        this.userService.deleteAll();
+        return "成功";
+    }
+
+    @GetMapping("execute")
+    @ApiOperation(value = "execute方法", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object execute() {
+        return this.userService.execute();
+    }
+
+    @PostMapping("executeSaveUserEntity")
+    @ApiOperation(value = "execute2方法", notes = "HibernateTemplate", httpMethod = "POST")
+    public Object executeSaveUserEntity(@RequestBody UserEntity userEntity) {
+        return this.userService.executeSaveUserEntity(userEntity);
+    }
+
+    @GetMapping("findByExample")
+    @ApiOperation(value = "Example查询所有数据", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findByExample() {
+        return this.userService.findByExample();
+    }
+
+    @GetMapping("findByExample2")
+    @ApiOperation(value = "Example查询所有数据-分页", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findByExample(int firstResult, int maxResults) {
+        return this.userService.findByExample(firstResult,maxResults);
+    }
+
+    @GetMapping("findByCriteria")
+    @ApiOperation(value = "Criteria查询所有数据", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findByCriteria() {
+        return this.userService.findByCriteria();
+    }
+
+    @GetMapping("findPageByCriteria")
+    @ApiOperation(value = "Criteria查询所有数据-分页", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findPageByCriteria(int firstResult, int maxResults) {
+        return this.userService.findPageByCriteria(firstResult,maxResults);
+    }
+
+    @GetMapping("findByCriteria2")
+    @ApiOperation(value = "Criteria根据属性查询数据-分页", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findByCriteria2(String propertyName, String propertyValue, int firstResult, int maxResults) {
+        return this.userService.findByCriteria(propertyName,propertyValue,firstResult,maxResults);
+    }
+
+    @GetMapping("findByCriteria3")
+    @ApiOperation(value = "Criteria名字模糊查询数据并按年龄区间和年龄大小获取", notes = "HibernateTemplate", httpMethod = "GET")
+    public Object findByCriteria3(@RequestParam int low,@RequestParam int high) {
+        return this.userService.findByCriteria(low,high);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- spring5x-hibernate5-base/src/main/java/com/zja/service/Impl/UserServiceImpl.java
+++ spring5x-hibernate5-base/src/main/java/com/zja/service/Impl/UserServiceImpl.java
@@ -2,31 +2,314 @@ package com.zja.service.Impl;
 
 import com.zja.entity.UserEntity;
 import com.zja.service.UserService;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.orm.hibernate5.HibernateCallback;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author ZhengJa
  * @description
  * @data 2019/11/19
  */
-@Service
+@Service("userServiceImpl")
 public class UserServiceImpl implements UserService {
 
     @Resource
     private HibernateTemplate hibernateTemplate;
 
-    @Override
-    public Object getuser(Integer id) {
-        return hibernateTemplate.get(UserEntity.class,id);
-    }
 
+    /*********** save/merge存数据 *************/
+
+    /**
+     * save保存-成功
+     *
+     * @param userEntity
+     */
     @Override
-    public Object saveUser(UserEntity userEntity){
+    public Object saveEntity(UserEntity userEntity) {
         userEntity.setCreateTime(new Date());
         return hibernateTemplate.save(userEntity);
     }
+
+    /**
+     * save保存-成功
+     *
+     * @param userEntity
+     */
+    @Override
+    public Object saveEntity2(UserEntity userEntity) {
+        userEntity.setCreateTime(new Date());
+        return hibernateTemplate.save("userEntity", userEntity);
+    }
+
+    /**
+     * merge保存-成功 :存在则更新，不存在则保存
+     * 使用merge的时候，执行完成，我们提供的对象A还是脱管状态，hibernate或者new了一个B，或者检索到
+     * 一个持久对象B，并把我们提供的对象A的所有的值拷贝到这个B，执行完成后B是持久状态，而我们提供的A还是托管状态
+     *
+     * @param userEntity 实体类
+     */
+    @Override
+    public UserEntity mergeEntity(UserEntity userEntity) {
+        userEntity.setCreateTime(new Date());
+        return hibernateTemplate.merge(userEntity);
+    }
+
+    /**
+     * merge保存-成功 :存在则更新，不存在则保存
+     *
+     * @param userEntity 实体类
+     */
+    @Override
+    public UserEntity mergeEntity2(UserEntity userEntity) {
+        userEntity.setCreateTime(new Date());
+        return hibernateTemplate.merge("userEntity", userEntity);
+    }
+
+
+    /*********** get/load存取单条数据 *************/
+    //get和load的根本区别:
+    // hibernate对于load方法认为该数据在数据库中一定存在，可以放心的使用代理来延迟加载，如果在 使用过程中发现了问题，就抛异常；
+    // 对于get方法，hibernate一定要获取到真实的数据，否则返回null
+
+    /**
+     * 根据id获取用户信息-成功
+     * 查询数据不存在，返回null,推荐使用, 不存在延迟加载问题，不采用lazy机制的
+     *
+     * @param id
+     */
+    @Override
+    public UserEntity getEntity(Integer id) {
+        return hibernateTemplate.get(UserEntity.class, id);
+    }
+
+    /**
+     * load查询单条数据，查询数据不存在，报异常，不推荐使用,存在延迟加载问题-失败
+     *
+     * @param id
+     */
+    @Override
+    public UserEntity loadEntity(Integer id) {
+        return hibernateTemplate.load(UserEntity.class, id);
+    }
+
+    /**
+     * loadAll查询所有用户信息-成功
+     *
+     * @param
+     */
+    @Override
+    public List<UserEntity> getListEntity() {
+        return hibernateTemplate.loadAll(UserEntity.class);
+    }
+
+    /**
+     * 根据id查询-失败
+     *
+     * @param id
+     */
+    @Override
+    public UserEntity getEntityName(Integer id) {
+        Object userEntity = hibernateTemplate.get("userEntity", id);
+        System.out.println("userEntity: " + userEntity);
+        return (UserEntity) hibernateTemplate.get("userEntity", id);
+    }
+
+    /**
+     * 分页查询-不推荐用-成功
+     */
+    @Override
+    public List<UserEntity> getPageByConditions() {
+        //hibernateTemplate单例,分页会有问题,执行一次分页后,其它的所有查询都是分页后效果
+        hibernateTemplate.setMaxResults(5);
+        List<UserEntity> loadAll = hibernateTemplate.loadAll(UserEntity.class);
+        hibernateTemplate.setMaxResults(0);
+        return loadAll;
+    }
+
+    /*********** 更新 *************/
+
+    /**
+     * 更新-成功
+     *
+     * @param userEntity
+     */
+    @Override
+    public void updateEntity(UserEntity userEntity) {
+        hibernateTemplate.update(userEntity);
+    }
+
+    /**
+     * 保存或更新：id不存在则保存，存在则更新-成功
+     *
+     * @param userEntity
+     */
+    @Override
+    public void saveOrUpdate(UserEntity userEntity) {
+        hibernateTemplate.saveOrUpdate(userEntity);
+    }
+
+    /*********** 删除 *************/
+
+    /**
+     * 删除单条数据-成功
+     *
+     * @param id
+     */
+    @Override
+    public void deleteByEntity(Integer id) {
+        hibernateTemplate.delete(getEntity(id));
+    }
+
+    /**
+     * 删除全部数据-成功
+     *
+     * @param
+     */
+    @Override
+    public void deleteAll() {
+        List<UserEntity> userEntityList = findByCriteria();
+        hibernateTemplate.deleteAll(userEntityList);
+    }
+
+
+    /*********** hibernateTemplate执行原生sql脚本 *************/
+
+    /**
+     * HiberateTemplate提供的方法不能满足要求时才使用execute方法 执行sql -成功
+     */
+    @Override
+    public Object execute() {
+        Object o = this.hibernateTemplate.execute(new HibernateCallback<Object>() {
+            @Override
+            public Object doInHibernate(Session session) throws HibernateException {
+                //创建空表mysql_ok
+                session.createSQLQuery(
+                        "DROP TABLE IF EXISTS `mysql_ok`;")
+                        .executeUpdate();
+                int i = session.createSQLQuery(
+                        "CREATE TABLE `mysql_ok` (`ok`  varchar(3) NOT NULL ,PRIMARY KEY (`ok`));")
+                        .executeUpdate();
+                System.out.println("成功创建空表mysql_ok ：i=" + i);
+                return i;
+            }
+        });
+        return o;
+    }
+
+    /**
+     * 保存实体类-成功
+     * @param userEntity
+     */
+    @Override
+    public Object executeSaveUserEntity(UserEntity userEntity) {
+        Object o = this.hibernateTemplate.execute(new HibernateCallback<Object>() {
+            @Override
+            public Object doInHibernate(Session session) throws HibernateException {
+                //创建空表mysql_ok
+                Serializable save = session.save(userEntity);
+                System.out.println("id=" + save);
+                return save;
+            }
+        });
+        return o;
+    }
+
+    /*********** findByExample查询数据 *************/
+
+    /**
+     * 根据条件查询-成功
+     */
+    @Override
+    public List<UserEntity> findByExample() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("小刘");
+        userEntity.setAge(20);
+        //必须 符合的条件但是这 "username=小刘 and age=20" 两个条件时并列的（象当于sql中的and)
+        return hibernateTemplate.findByExample(userEntity);
+    }
+
+    /**
+     * 根据调条件查询并分页-成功
+     *
+     * @param firstResult 页码 从0开始
+     * @param maxResults  每页显示多少条数据，等于0或负数则查询所有
+     */
+    @Override
+    public List<UserEntity> findByExample(int firstResult, int maxResults) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("小刘");
+        userEntity.setAge(20);
+        return hibernateTemplate.findByExample(userEntity, firstResult, maxResults);
+    }
+
+    /*********** findByExample查询数据 *************/
+
+    /**
+     * 查询所有数据 -成功
+     */
+    @Override
+    public List<UserEntity> findByCriteria() {
+        //DetachedCriteria：离线条件查询对象，该对象的创建不需要session对象
+        DetachedCriteria criteria = DetachedCriteria.forClass(UserEntity.class);
+        return (List<UserEntity>) hibernateTemplate.findByCriteria(criteria);
+    }
+
+    /**
+     * 分页查询-成功
+     *
+     * @param firstResult 页码 从0开始
+     * @param maxResults  每页显示多少条数据，等于0或负数则查询所有
+     */
+    @Override
+    public List<UserEntity> findPageByCriteria(int firstResult, int maxResults) {
+        //firstResult 第几页，maxResults每页显示多少条
+        DetachedCriteria criteria = DetachedCriteria.forClass(UserEntity.class);
+        return (List<UserEntity>) hibernateTemplate.findByCriteria(criteria, firstResult, maxResults);
+    }
+
+    /**
+     * 根据属性名称和属性值查询-成功
+     *
+     * @param propertyName  类的属性名称
+     * @param propertyValue 属性值
+     * @param firstResult   页码 从0开始
+     * @param maxResults    每页显示多少条数据，等于0或负数则查询所有
+     */
+    @Override
+    public List<UserEntity> findByCriteria(String propertyName, Object propertyValue, int firstResult, int maxResults) {
+        DetachedCriteria criteria = DetachedCriteria.forClass(UserEntity.class);
+        //根据属性名称和属性的value值查询
+        criteria.add(Restrictions.eq(propertyName, propertyValue));
+        return (List<UserEntity>) hibernateTemplate.findByCriteria(criteria, firstResult, maxResults);
+    }
+
+    /**
+     * 模糊查询按年龄区间和大小排序-成功
+     *
+     * @param low  低
+     * @param high 高  low --> high 从低到高，顺序一定要正确，否则查询会有问题，或查询不到数据
+     */
+    @Override
+    public List<UserEntity> findByCriteria(int low, int high) {
+        //
+        DetachedCriteria criteria = DetachedCriteria.forClass(UserEntity.class);
+        //根据属性名称和属性的value值查询
+        criteria.add(Restrictions.like("userName", "小%"));
+        criteria.add(Restrictions.between("age", low, high));
+        //根据属性名称进行排序
+        criteria.addOrder(Order.desc("age"));
+        return (List<UserEntity>) hibernateTemplate.findByCriteria(criteria);
+    }
+
 }
