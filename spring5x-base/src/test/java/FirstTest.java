004a4e2b7121e311bd257a5d89770d923e21df66
zhengja <zhengja@dist.com.cn>
2020-01-06 13:54:08 +0800

    feat(*):新增测试

---
+++ spring5x-base/src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Date: 2020-01-06 13:46
+ * Author: zhengja
+ * Email: zhengja@dist.com.cn
+ * Desc：第一个测试用例
+ */
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void test(){
+        System.out.println("sout 输出信息");
+    }
+}
